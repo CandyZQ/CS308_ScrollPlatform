671992b511f3fa7b289c5d60d9ad149620a5b689
QIAOYI FANG <QF30@duke.edu>
2020-05-04 01:08:04 -0400

    added the masterpiece

---
+++ src/ooga/view/engine/graphics/animation/Animation2D_masterpiece.java
@@ -0,0 +1,110 @@
+package ooga.view.engine.graphics.animation;
+
+import ooga.view.engine.graphics.Material;
+
+/**
+ * an class that implements 2D animation and extends from the Animation class
+ * In addition to the basic mechanism provided in its parent class,
+ * it implemented the specific nuances to 2D animation such as to load, update or return the animation texture at the current frame.
+ * @author qiaoyi fang
+ */
+public class Animation2D_masterpiece extends Animation{
+
+  private Material[] animatedFrames;
+
+  /**
+   * constructor
+   * @param cnt total amount of frames
+   * @param fps frame per second
+   * @param dir directory
+   */
+  public Animation2D_masterpiece(int cnt, int fps, String dir) {
+    super(cnt,fps);
+    this.animatedFrames = new Material[cnt];
+
+    for(int i=0; i<cnt;i++){
+      String spritePath = String.format("%s/%s_.png", dir.replace("\\", "/"), i);
+      this.animatedFrames[i] = new Material(spritePath);
+      this.animatedFrames[i].createTexture();
+    }
+  }
+
+  /**
+   * constructor
+   * @param cnt total amount of frames
+   * @param fps frame per second
+   * @param dir directory
+   * @param action animation state
+   */
+  public Animation2D_masterpiece(int cnt, int fps, String dir, String action) {
+    super(cnt,fps);
+    this.animatedFrames = new Material[cnt];
+
+    for(int i=0; i<cnt;i++){
+      String spritePath = String.format("%s%s%s.png", dir, action.toLowerCase(), i+1);
+      this.animatedFrames[i] = new Material(spritePath);
+      this.animatedFrames[i].createTexture();
+    }
+  }
+
+  /**
+   * empty constructor
+   * @param cnt total amount of frame
+   * @param fps frame per second
+   */
+  public Animation2D_masterpiece(int cnt, int fps) {
+    super(cnt, fps);
+    this.animatedFrames = new Material[cnt];
+  }
+
+  /**
+   * combines two animations
+   * @param animation_1 the first animation
+   * @param animation_2 the second animation
+   * @return the combined animation
+   */
+  public static Animation2D_masterpiece combineAnimations(Animation2D_masterpiece animation_1, Animation2D_masterpiece animation_2){
+
+    int totalFrames = animation_1.getFrameAmount()+animation_2.getFrameAmount();
+    Animation2D_masterpiece combined = new Animation2D_masterpiece(totalFrames, DEFAULT_FPS_2D);
+
+    int idx = 0;
+    for (int i=0; i<animation_1.getFrameAmount(); i++){
+      combined.setAnimatedFrame(idx++, animation_1.getAnimatedFrame(i));
+    }
+
+    for (int i=0; i<animation_2.getFrameAmount(); i++){
+      combined.setAnimatedFrame(idx++, animation_2.getAnimatedFrame(i));
+    }
+
+    return combined;
+  }
+
+  /**
+   * sets the content of an animation frame
+   * @param idx index
+   * @param frame frame content (texture)
+   */
+  public void setAnimatedFrame(int idx, Material frame){
+    this.animatedFrames[idx] = frame;
+  }
+
+  /**
+   * gets the content of an animation frame
+   * @param idx index
+   * @return frame content (texture)
+   */
+  public Material getAnimatedFrame(int idx){
+    return this.animatedFrames[idx];
+  }
+
+  /**
+   * gets the current frame
+   * @return frame content (texture)
+   */
+  public Material getCurrentFrame(){
+    boolean is2D = true;
+    int framePointer = updateFramePointer(is2D);
+    return framePointer==-1?null: animatedFrames[framePointer];
+  }
+}
---
+++ src/ooga/view/engine/graphics/animation/Animation_masterpiece.java
@@ -0,0 +1,86 @@
+package ooga.view.engine.graphics.animation;
+
+import ooga.view.engine.utils.Timer;
+
+/**
+ * the class extracted and implemented the shared animation mechanism for both 2D and 3D,
+ * which includes the implementation of updating the location of the frame pointer according to the timer,
+ * resetting the animation after its completion, and so on.
+ * @author qiaoyi fang
+ **/
+abstract public class Animation_masterpiece {
+
+  protected final static int DEFAULT_FPS_2D = 18;
+  protected final static int DEFAULT_FPS_3D = 23;
+  protected final static double HARD_SET_2D_TIME = 1.0/ (double) DEFAULT_FPS_2D;
+  protected final static double HARD_SET_3D_TIME = 1.0/ (double) DEFAULT_FPS_3D;
+
+  protected int framePointer;
+  protected int frameAmount;
+
+  protected double elapsedTime;
+  protected double currentTime;
+  protected double lastTime;
+  protected double fps;
+
+  public Animation_masterpiece(int cnt, int fps){
+    resetAnimation();
+    this.fps = 1.0 / fps;
+    this.frameAmount = cnt;
+  }
+
+  /**
+   * resets the animation
+   */
+  public void resetAnimation(){
+    this.framePointer = 0;
+    this.elapsedTime = 0;
+    this.currentTime = 0;
+    this.lastTime = Timer.getTime();
+  }
+
+  /**
+   * get the amount of frames
+   * @return total amount of frames
+   */
+  public int getFrameAmount(){return frameAmount;}
+
+  /**
+   * updates the current frame pointer
+   * @param is2D whether the animation is 2D (determines the FPS)
+   * @return the pointer to the current frame
+   */
+  public int updateFramePointer(boolean is2D){
+    this.currentTime = Timer.getTime();
+    this.elapsedTime += currentTime - lastTime;
+
+    if (elapsedTime >= fps || elapsedTime >= (is2D? HARD_SET_2D_TIME:HARD_SET_3D_TIME) ){
+      elapsedTime = 0;
+      framePointer++;
+      this.lastTime = currentTime;
+    }
+
+    if (framePointer >= frameAmount)
+    {
+      framePointer = 0;
+      return -1;
+    }
+
+    return framePointer;
+  }
+
+  /**
+   * for testing
+   */
+  public void printStatus(){
+    System.out.println(framePointer);
+    System.out.println(frameAmount);
+    System.out.println(currentTime);
+    System.out.println(elapsedTime);
+    System.out.println(lastTime);
+    System.out.println(currentTime - lastTime);
+    System.out.println(fps);
+    System.out.println();
+  }
+
+}
